f61f6f37c9ac6aa0bd200250af77dc656fa715ba
Bushman-31 <513323@vistacollege.nl>
2023-05-12 14:02:15 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -9,10 +9,47 @@
 <body>
 <form action="verify.php" method="post"> 
 <label> User Name:<br> </label>
-<input type="text" name="username"><br><br> 
+<input type="text" Uname="username"><br><br> 
 <label>Password:<br> </label>
-<input type="password" name="password"><br><br> 
-<input type="submit" name="submit" value="Login"> 
+<input type="password" password="password"><br><br> 
+<input type="submit" name="submit" value="Login">
 </form> 
 </body>
 </html>
+
+<?php
+    // getting all values from the HTML form
+    if(isset($_POST['submit']))
+    {
+        $Uname = $_POST['username'];
+        $password = $_POST['password'];
+    }
+
+$servername = "challenge10";
+$username = "challenge10";
+$password = "12345";
+$dbname = "challenge10";
+
+    // creating a connection
+    $con = mysqli_connect($host, $username, $password, $dbname);
+
+    // to ensure that the connection is made
+    if (!$con)
+    {
+        die("Connection failed!" . mysqli_connect_error());
+    }
+
+    // using sql to create a data entry query
+    $sql = "INSERT INTO contactform_entries (id, fname, lname, email) VALUES ('0', '$fname', '$lname', '$email')";
+  
+    // send query to the database to add values and confirm if successful
+    $rs = mysqli_query($con, $sql);
+    if($rs)
+    {
+        echo "Entries added!";
+    }
+  
+    // close connection
+    mysqli_close($con);
+
+?>
